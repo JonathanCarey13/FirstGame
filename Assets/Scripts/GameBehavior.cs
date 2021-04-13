95ff024e586d1b3038dd95b715f296f1e79c0cb3
JonathanCarey13 <jonathan.carey1995@gmail.com>
2021-04-12 20:58:01 -0400

    added gameManager script/obj

---
+++ Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehavior : MonoBehaviour
+{
+    private int _itemsCollected = 0;
+
+    public int Items
+    {
+        get { return _itemsCollected; }
+        set 
+        { 
+            _itemsCollected = value;
+            Debug.LogFormat("Items: {0}", _itemsCollected);
+        }
+    }
+
+    private int _playerHP = 10;
+
+    public int HP
+    {
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
+    }
+
+    private string _firstName;
+    public string FirstName
+    {
+        get
+        {
+            return _firstName;
+        }
+        set
+        {
+            _firstName = value;
+        }
+    }
+
+}
--- Assets/Scripts/ItemBehavior.cs
+++ Assets/Scripts/ItemBehavior.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour
 {
+    public GameBehavior gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
+    }
     void OnCollisionEnter (Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Item Collected!");
+            gameManager.Items += 1;
         }
     }
 }
