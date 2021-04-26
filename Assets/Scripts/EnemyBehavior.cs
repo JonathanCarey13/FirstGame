9d7e302ace896bf1ee0452e70f673687124ed018
JonathanCarey13 <jonathan.carey1995@gmail.com>
2021-04-25 21:58:23 -0400

    added UI/enemy patrol

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,9 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public Transform patrolRoute;
+    public List<Transform> locations;
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.2f && !agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if (locations.Count == 0)
+            return;
+        agent.destination = locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % locations.Count;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if(other.name == "Player")
--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameBehavior : MonoBehaviour
 {
+    public string labelText = "Collect all 4 items to win!";
+    public int maxItems = 4;
     private int _itemsCollected = 0;
+    public bool showWinScreen = false;
 
     public int Items
     {
@@ -12,8 +16,19 @@ public class GameBehavior : MonoBehaviour
         set 
         { 
             _itemsCollected = value;
-            Debug.LogFormat("Items: {0}", _itemsCollected);
+
+            if (_itemsCollected >= maxItems)
+            {
+                labelText = "You've found all the items!";
+                showWinScreen = true;
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
+            }
         }
+
     }
 
     private int _playerHP = 10;
@@ -28,17 +43,21 @@ public class GameBehavior : MonoBehaviour
         }
     }
 
-    private string _firstName;
-    public string FirstName
+    private void OnGUI()
     {
-        get
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health: " + _playerHP);
+
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
+
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+
+        if (showWinScreen)
         {
-            return _firstName;
-        }
-        set
-        {
-            _firstName = value;
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "YOU WON!"))
+            {
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1.0f;
+            }
         }
     }
-
 }
